6528e9eaf28537f7f8480e6ace6dfadcbc880c28
Zach Bierker <zachbier79@gmail.com>
2026-03-31 16:05:00 -0400

    Update state_interface.py
    Hey guys Zach here this code is lowkey just amirs but im the one whos pushing it so its mine now

--- python/state_interface.py
+++ python/state_interface.py
@@ -1,9 +1,6 @@
 from arduino.app_utils import *
 import numpy as np
 
-numOfModes = 2
-mode = 0
-
 class LedState:
     def calculate_array(self, arr):
         a = np.array(arr)
@@ -14,12 +11,12 @@ class LedController:
         self.states = states
 
     def update(self, mode):
+        mode = mode % len(self.states)  # wrap around safely
         encoded = ",".join(str(x) for x in self.states[mode].calculate_array())
         Bridge.call("drawMatrix", encoded)
 
-
 class Smile(LedState):
-    def calculate_array():
+    def calculate_array(self):          # ← add 'self'
         arr = [[0,0,0,0,0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0],
                 [0,0,1,1,0,0,0,0,0,1,1,0,0],
@@ -28,18 +25,4 @@ class Smile(LedState):
                 [0,0,1,0,0,0,0,0,0,0,1,0,0],
                 [0,0,0,1,1,1,1,1,1,1,0,0,0],
                 [0,0,0,0,0,0,0,0,0,0,0,0,0]]
-        return super().calculate_array(arr)
-
-def changeMode(num):
-    global mode 
-    mode = num%numOfModes
-    print(mode)
-
-smile = Smile()
-states = [smile]
-mode = 0
-numOfModes = len(states)
-
-name = LedController(states)
-while True:
-    name.update(mode)
+        return super().calculate_array(arr)  # ← this was already correct
