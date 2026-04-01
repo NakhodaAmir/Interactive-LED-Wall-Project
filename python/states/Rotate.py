04807f121eec4a677b943d96dd83762d399c9fa2
Zach Bierker <zachbier79@gmail.com>
2026-04-01 17:53:33 -0400

    Update Rotate.py
    Pushed wrong thing and fixed that

--- python/states/Rotate.py
+++ python/states/Rotate.py
@@ -1,16 +1,18 @@
 from classes.LedState import LedState
 
-class Smile(LedState):
+class Rotate(LedState):
     def __init__(self):
-        super().__init__(0)
+        super().__init__(9)
 
-    def calculate_array(self, step):
-        arr =  [[0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,1,1,0,0,0,0,0,1,1,0,0],
-                [0,0,1,1,0,0,0,0,0,1,1,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,1,0,0,0,0,0,0,0,1,0,0],
-                [0,0,0,1,1,1,1,1,1,1,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0]]
+    def calculate_array(self, step: int):
+        arr = [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
+        for _ in range(step % self.max_steps):
+            arr = [list(reversed(arr)) for arr in zip(*arr)]
         return super().calculate_array(arr)
