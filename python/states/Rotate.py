c7c8cf5d1237949bc8afcb1663056d08708b6f0a
Amir Nakhoda <fmn00rdin@gmail.com>
2026-04-01 01:04:09 -0400

    Created new LedState
    Led State that rotates 90 degrees

---
+++ python/states/Rotate.py
@@ -0,0 +1,18 @@
+from python.classes.LedState import LedState
+
+class Rotate(LedState):
+    def __init__(self):
+        super().__init__(9)
+
+    def calculate_array(self, step: int):
+        arr = [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
+        for _ in range(step % self.max_steps):
+            arr = [list(reversed(arr)) for arr in zip(*arr)]
+        return super().calculate_array(arr)
