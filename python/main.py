97209dedd3547fbf7608f24e37578e8da56636ce
Zach Bierker <zachbier79@gmail.com>
2026-04-11 13:15:09 -0400

    Update main.py
    Added audio test mode and added a debug function
    Had weird issue with the rotate mode taking priority over everything so removed it temporarily

--- python/main.py
+++ python/main.py
@@ -1,11 +1,21 @@
 from states.SmileTest import Smile
 from states.FrownTest import Frown
 from states.Rotate import Rotate
+from states.AudioTest import AudioTest
 from classes.LedController import LedController
+from classes.MicController import getAudio
+from time import sleep
 
 def main():
-    controller = LedController([Rotate(), Smile(), Frown()])
+#Use for debugging    
+#    while True:
+#        for name, level in getAudio().items():
+#            print(f"{name:<10}: {level:.1f}")
+#        print("---")
+#        sleep(.1)
+    controller = LedController([AudioTest(), Smile(), Frown()])
     controller.update()
+
 
 if __name__ == "__main__":
     main()
