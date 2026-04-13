6e979981fff828bf7edd4693e2fa31cb34c1d8a0
Zach Bierker <zachbier79@gmail.com>
2026-04-13 15:33:07 -0400

    Update main.py
    Added button

--- python/main.py
+++ python/main.py
@@ -1,20 +1,38 @@
-from states.SmileTest import Smile
-from states.FrownTest import Frown
-from states.Rotate import Rotate
-from states.AudioTest import AudioTest
-from classes.LedController import LedController
-from classes.MicController import getAudio
-from time import sleep
+Arduino_LED_Matrix matrix;
+uint8_t LedMatrix[104];
 
-def main():
-#Use for debugging    
-#    while True:
-#        for name, level in getAudio().items():
-#            print(f"{name:<10}: {level:.1f}")
-#        print("---")
-#        sleep(.1)
-    controller = LedController([AudioTest(), Smile(), Frown()])
-    controller.update()
+int currentMode = 0;
+int lastButtonState = 0;
 
-if __name__ == "__main__":
-    main()
+void drawMatrixBuiltIn(String encoded) {
+  int i = 0;
+  while (encoded.length() > 0 && i < 104) {
+    int comma = encoded.indexOf(',');
+    if (comma == -1) { LedMatrix[i] = encoded.toInt(); break; }
+    LedMatrix[i] = encoded.substring(0, comma).toInt();
+    encoded = encoded.substring(comma + 1);
+    i++;
+  }
+  matrix.draw(LedMatrix);
+}
+
+void setup() {
+  matrix.begin();
+  matrix.setGrayscaleBits(1);
+  Serial.begin(9600);
+  Bridge.begin();
+  Bridge.provide("drawMatrix", drawMatrixBuiltIn);
+  analogReadResolution(10); 
+}
+
+void loop() {
+  Bridge.update();
+  int currentButtonState = analogRead(A0); 
+  if (currentButtonState > 1020 && lastButtonState <= 1020) {
+    currentMode++;
+    Bridge.call("ChangeMode", currentMode);
+  }
+  lastButtonState = currentButtonState;
+  Serial.println(currentButtonState);
+  delay(10);
+}
