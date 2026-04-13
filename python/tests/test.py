39e9487696a26b7421319998dcbfb8b97a9268de
Amir Nakhoda <fmn00rdin@gmail.com>
2026-04-13 12:33:46 -0400

    Update test.py
    Testing new class

--- python/tests/test.py
+++ python/tests/test.py
@@ -2,9 +2,11 @@ from python.states.SmileTest import Smile
 from python.states.FrownTest import Frown
 from python.states.Rotate import Rotate
 from python.classes.LedController import LedController
+from states.FallingAstroid import FallingAstroid
+
 
 def main():
-    controller = LedController([Rotate(), Smile(), Frown()])
+    controller = LedController([FallingAstroid()])
     controller.update_test()
 
 if __name__ == "__main__":
