145252ada335c4c129f251d3a1cd03dda30eac51
Zach Bierker <zachbier79@gmail.com>
2026-04-13 15:31:25 -0400

    Update AudioTest.py
    Fixed array initializing and made the bars show up in a random order and fill all columns

--- python/states/AudioTest.py
+++ python/states/AudioTest.py
@@ -1,25 +1,23 @@
 from classes.MicController import getAudio
 from classes.LedState import LedState
+import random
 
 class AudioTest(LedState):
     def __init__(self):
         super().__init__(0)
+        self.column_order = list(range(13))
+        random.shuffle(self.column_order)
 
-    def calculate_array(self, step): #If you are seeing this I lowk dont know how to do an all 0 array in python so if you know def fix thanks
-        arr =  [[0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0,0,0,0]]
-
+    def calculate_array(self, step):
+        arr = [[0] * 13 for _ in range(8)]
         levels = getAudio()
         keys = ["Sub Bass", "Bass", "Mid", "Treble", "Air"]
-        for i in range(5):
-            num_leds = (levels[keys[i]] / 100) * 8
+
+        for i in range(13):
+            col = self.column_order[i] 
+            num_leds = (levels[keys[i % 5]] / 100) * 8
             for j in range(len(arr)):
                 if j >= (8 - num_leds):
-                    arr[j][i] = 1
+                    arr[j][col] = 1
+
         return super().calculate_array(arr)
