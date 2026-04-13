fc163f6aa7cd746e74c1e9f55bce5655a5c9ef0f
Zach Bierker <zachbier79@gmail.com>
2026-04-13 16:38:57 -0400

    Update LedController.py

--- python/classes/LedController.py
+++ python/classes/LedController.py
@@ -1,5 +1,6 @@
 from arduino.app_utils import *
 from time import sleep
+#from classes.MicController import readAudio
 
 # main class that controls all led states.
 # states is an array of all the led states
@@ -11,34 +12,32 @@ class LedController:
         self.num_of_modes = len(self.states)
         self.mode = 0
         Bridge.provide("ChangeMode", self.changeMode)
-        self.current_frame = 0
-        self.max_frame = self.states[self.mode].max_frames + 1
 
     def changeMode(self, num):
         self.mode = num % self.num_of_modes
-        self.current_frame = 0
-        self.max_frame = self.states[self.mode].max_frames + 1
+        print("Changed Mode!")
 
-    # main loop of the controller
+    
+    #main loop of the controller
     # runs a for loop for the number of max_steps of the led state
     # to show each iteration of the led state
     def update(self):
         while True:
-            if self.current_frame >= self.max_frame:
-                self.current_frame = 0
-            encoded = ",".join(str(x) for x in self.states[self.mode].calculate_array(self.current_frame))
+            encoded = ",".join(str(x) for x in self.states[self.mode].calculate_array(0))
             Bridge.call("drawMatrix", encoded)
-            sleep(0.01)
-            self.current_frame += 1
+            if(self.mode == 1):
+                sleep(0.025)
+            else:
+                sleep(0.01)
+            
 
+    #testing loop that only prints out one cycle of the led state into the terminal prettily
     def update_test(self):
-        while True:
-            if self.current_frame >= self.max_frame:
-                self.current_frame = 0
-            encoded = ",".join(str(x) for x in self.states[self.mode].calculate_array(self.current_frame))
-            chunks = [encoded[i:i + 26] for i in range(0, len(encoded), 26)]
+        for step in range(self.states[self.mode].max_frames + 1):
+            encoded = ",".join(str(x) for x in self.states[self.mode].calculate_array(step))
+            #Pretty Prints the design in the terminal
+            chunks = [encoded[i:i+26] for i in range(0, len(encoded), 26)]
             for chunk in chunks:
                 print(chunk)
-            sleep(1)
-            self.current_frame += 1
             print("--------------------------")
+            sleep(1)
