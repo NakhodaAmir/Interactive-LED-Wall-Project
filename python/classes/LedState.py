6d85977b9f7fa794f8fde1ccae5a9a53a16b1f4d
MaxBellenbaum <buffalomaxb@gmail.com>
2026-04-15 00:01:24 -0400

    Added method to change colors
    Implemented a method in the parent class that implements colors

--- python/classes/LedState.py
+++ python/classes/LedState.py
@@ -7,6 +7,17 @@ class LedState:
     def __init__(self, frame_rate):
         self.frame_rate = frame_rate
 
+    # takes in a 2d array and 2 1d arrays of rgb codes and returns a 3d array of rgb codes
+    def coloredArray(self, arr, primary, background):
+        coloredArray = np.copy(arr)
+        for row in coloredArray:
+            for num in coloredArray:
+                if(num == 1):
+                    num = primary
+                else:
+                    num = background
+        return coloredArray
+
     #takes in a 2d array and return a 1d array
     def calculate_array(self, arr):
         a = np.array(arr)
