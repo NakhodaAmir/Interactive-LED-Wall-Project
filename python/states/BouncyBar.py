143837b94917391f694472d4395f03ec3813ac8e
MaxBellenbaum <buffalomaxb@gmail.com>
2026-04-14 20:05:26 -0400

    added bouncybar state

--- python/states/BouncyBar.py
+++ python/states/BouncyBar.py
@@ -18,14 +18,47 @@ class BassBar(LedState):
     def __init__(self):
         super().__init__(0)
 
-    def bassBar(self):
+    def bouncyBar(self):
         self.audio = getAudio()
-        arr = np.zeros(self.width, self.height)
-        row = self.mapVoltage(self.currentVolume)
-        shifted = np.zeros_like(arr) # creates new array for shift command
-        shifted[:, :1] = arr[:, 1:] # copies arr to shifted with everything shifted 1 column right
-        for i in range(self.height):
-            shifted[i][0] = row[i]
+        arr = np.zeros(self.height, self.width)
+
+        """ # For testing on 19x14 full LED wall
+        arr[0] = self.mapVoltage(self.currentSubBass / 2)
+        arr[1] = self.mapVoltage(self.currentSubBass)
+        arr[2] = self.mapVoltage(self.currentSubBass)
+        arr[3] = self.mapVoltage(self.currentSubBass / 2)
+        arr[4] = self.mapVoltage(self.currentBass / 2)
+        arr[5] = self.mapVoltage(self.currentBass)
+        arr[6] = self.mapVoltage(self.currentBass)
+        arr[7] = self.mapVoltage(self.currentBass / 2)
+        arr[8] = self.mapVoltage(self.currentMid / 2)
+        arr[9] = self.mapVoltage(self.currentMid)
+        arr[10] = self.mapVoltage(self.currentMid)
+        arr[11] = self.mapVoltage(self.currentMid / 2)
+        arr[12] = self.mapVoltage(self.currentTreble / 2)
+        arr[13] = self.mapVoltage(self.currentTreble)
+        arr[14] = self.mapVoltage(self.currentTreble)
+        arr[15] = self.mapVoltage(self.currentTreble / 2)
+        arr[16] = self.mapVoltage(self.currentAir / 2)
+        arr[17] = self.mapVoltage(self.currentAir)
+        arr[18] = self.mapVoltage(self.currentAir / 2)
+        """
+        # """ For testing on 13x8 Arduino display
+        arr[0] = self.mapVoltage(self.currentSubBass)
+        arr[1] = self.mapVoltage(self.currentSubBass / 2)
+        arr[2] = self.mapVoltage(self.currentBass / 2)
+        arr[3] = self.mapVoltage(self.currentBass)
+        arr[4] = self.mapVoltage(self.currentBass / 2)
+        arr[5] = self.mapVoltage(self.currentMid / 2)
+        arr[6] = self.mapVoltage(self.currentMid)
+        arr[7] = self.mapVoltage(self.currentMid / 2)
+        arr[8] = self.mapVoltage(self.currentTreble / 2)
+        arr[9] = self.mapVoltage(self.currentTreble)
+        arr[10] = self.mapVoltage(self.currentTreble / 2)
+        arr[11] = self.mapVoltage(self.currentAir / 2)
+        arr[12] = self.mapVoltage(self.currentAir)
+        # """
+        np.rot90(arr)
         return super().calculate_array(arr)
 
 
