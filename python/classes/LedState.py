9cca0965258bb86723a2dec326a7841ce683bc6c
MaxBellenbaum <buffalomaxb@gmail.com>
2026-04-15 20:59:17 -0400

    Added Calculate Spiral Array
    Added a method which takes a standard 2d array and reorders its elements to match the spiral pattern of the actual LED wall. Also added a commented out way to implement the method into current framework without changes.

--- python/classes/LedState.py
+++ python/classes/LedState.py
@@ -18,7 +18,35 @@ class LedState:
                     num = background
         return coloredArray
 
+    def calculateSpiralArray(self, arr): # need to finish grid later but this is the concept
+        count = 0
+        order = [
+            [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
+            [13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 33],
+            [12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 34],
+            [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 35],
+            [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36], # etc
+            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        for x in range(len(arr)):
+            for y in range(len(arr[x])):
+                for rw in order:
+                    for spot in rw:
+                        if spot == count:
+                            spot = arr[x][y]
+                            count += 1
+        return order
+
     #takes in a 2d array and return a 1d array
     def calculate_array(self, arr):
         a = np.array(arr)
+        #a = self.calculateSpiralArray(np.array(a))
         return a.ravel()
